d6cc87e113874c00ec10d0901847a0028647f9b9
iankingcoop <iankingcoop2@gmail.com>
2024-07-17 22:11:42 -0700

    scrap file

--- app.py
+++ app.py
@@ -9,12 +9,7 @@ import json
 
 
 
-# --- scrap initiate app ---
-# # Install the Slack app and get xoxb- token in advance
-# app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
-# if __name__ == "__main__":
-#     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
 
 
 # --- new initiate app ---
---
+++ scrap.py
@@ -0,0 +1,6 @@
+# --- scrap initiate app ---
+# # Install the Slack app and get xoxb- token in advance
+# app = App(token=os.environ["SLACK_BOT_TOKEN"])
+
+# if __name__ == "__main__":
+#     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
