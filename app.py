75b6257a5c44e6b841b4ca28376c4d8b5e0673af
iankingcoop <iankingcoop2@gmail.com>
2024-07-25 13:19:46 -0700

    misc

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
+import logging
 
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 # from config file import configurations?
+from slack_message_handler import SlackMessageHandler
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 import json
@@ -24,27 +26,19 @@ def init_app():
 
 
     #TODO should these values be init'd in the SlouchMessageHandler instead??
+    #TODO i think ill just pass them to SlouchMessageHandler... but should i just abstract these all into "slouch_settings"?
     ##### ----- access configuration file ----- #####
-    with open('config.json') as config_file:
-        config = json.load(config_file)
-
-    ##### ----- set security point of contact ----- #####
-    security_poc = config['security_poc'] # find user id... replace in config file
-
-    ##### ----- set expected working hours ----- #####
-    working_hours = config['working_hours'] # how to easily reference working hours in config file + code?
+    with open('slouch_settings.json') as config_file:
+        slouch_settings = json.load(config_file)
 
     ##### ----- access dlp rules file ----- #####
-    with open('dlp_rules.json') as config_file:
-        dlp_rules = json.load(config_file)
+    with open('dlp_rules.json') as dlp_file:
+        dlp_rules = json.load(dlp_file)
 
-    ##### ----- set DLP regex ----- #####
-    dlp_rules = dlp_rules['rules'] # how to easily reference working hours in config file + code?
 
     ##### ----- set relevant Slack channel ----- #####
-    channel_name = config['channel_name'] # how do I make this multipple??
+    channel_name = slouch_settings['channel_name'] # how do I make this multiple slack channels??
     # target_channels = []
-    api_key = config['api_key']
 
     ##### ----- retrieve "conversation ID" (ID for the workspace/channel where the app is listening...) ----- #####
     # slack_conv_id isn't hard coded... it could be, but these  next few lines help retrive the convo ID whether we're
@@ -63,4 +57,41 @@ def init_app():
     except SlackApiError as e:
         print(f"Error: {e}")
 
-    return app, slack_web_client, slack_conv_id
+    return app, slack_web_client, slack_conv_id, slouch_settings, dlp_rules
+
+
+def main():
+
+    app, slack_web_client, slack_conv_id, slouch_settings, dlp_rules = init_app()
+
+    msg_handler = SlackMessageHandler(slack_web_client, slack_conv_id, slouch_settings, dlp_rules)
+
+    ##### ----- slack bot event triggers ----- #####
+    # message: the main event trigger, an ask-dnr inbound AKA "message"
+    @app.event("message")  # https://api.slack.com/events/message.channels
+    def respond_channel_message(client, body, message, event, say):
+        if message.get("type") != "message":
+            logging.warning(
+                "Message is not of type ~message~. Maybe it's a block_actions event. Stopping."
+            )
+            return
+
+        if message.get("text") is None:
+            logging.warning(
+                "Message doesn't have any text. Not sure why... is it really a message? Stopping."
+            )
+            return
+
+        # TODO will it be able to monitor im messages? or no... just specific channels...
+        # if message.get("channel_type") == "im":
+        #     logging.warning("Not paying attention to IM messages right now.")
+        #     return
+
+        logging.warning("Looks like a message we can work with: {}".format(message))
+
+        # record the slack message text
+        msg_handler.content = message["text"]
+
+        msg_handler.scan_message(message["ts"])
+        msg_handler.post_faq_content(message["ts"])
+        msg_handler.initial_reply(message["ts"])
--- slack_message_handler.py
+++ slack_message_handler.py
@@ -1,24 +1,50 @@
+import re
 
+class SlackMessageHandler:
 
-
-
-class SlouchMessageHandler:
-
-    def __init__(self, slack_web_client, slack_conv_id):
+    def __init__(self, slack_web_client, slack_conv_id, slouch_settings, dlp_rules):
         self.client = slack_web_client
         self.conversation_id = slack_conv_id
         # content is overwritten when a listener matches a slack event
         self.content = None
-        self.security_poc?
+
+        ##### ----- set security point of contact ----- #####
+        self.security_poc = slouch_settings['security_poc'] # find user id... replace in slouch_settings file
+
+        ##### ----- set expected working hours ----- #####
+        self.working_hours = slouch_settings['working_hours'] # how to easily reference working hours in config file + code?
+
+        ##### ----- set DLP regex ----- #####
+        self.dlp_rules = dlp_rules['rules']
+
 
     # check to see if any DLP rules match
-    def scan_message():
-        pass
+    def scan_message(rule, slack_message):
+        if re.match(rule, slack_message):
+            return True
+        return False
+    
+    def check_dlp_pattern(self, slack_message):
+        for rule_name, pattern in self.dlp_rules['rules']:
+            if re.match(pattern, slack_message):
+                return "DLP rule \"f{rule_name}\" matched"
 
-    # alert the security POC if there's a match
-    def send_alert():
+
+        return False
+
+    # alert the security POC if there's a match -- in thread. "https://api.slack.com/methods/chat.postMessage" apparently its hard to start a DM.
+    def send_alert(self, body):
+        self.client.chat_postMessage(
+            channel=self.conversation_id,
+            thread_ts=body["container"]["message_ts"],
+            text="DLP rule matched :rotating_light: @{self_security_poc}", #how to @ a user using a user ID??
+        )
         pass
 
     # react to the message if there's a dlp match. react with an emoji and reply in thread?
-    def message_reaction():
-        pass
+    def message_reaction(self, body):
+        self.app.client.reactions_add(
+            channel=self.conversation_id,
+            timestamp=body["container"]["message_ts"],
+            name="rotating_light: @{self_security_poc}", #how to @ a user using a user ID??
+        )
